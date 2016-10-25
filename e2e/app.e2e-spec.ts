2f5a5f604147f9476f11c9f19f48e5e8eac27e47
angular-cli <angular-cli@angular.io>
2016-10-25 09:01:04 -0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { LajugWebsitePage } from './app.po';
+
+describe('lajug-website App', function() {
+  let page: LajugWebsitePage;
+
+  beforeEach(() => {
+    page = new LajugWebsitePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
